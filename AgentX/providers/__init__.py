537bad5363f512707ef13f55d6c54df0f5fab9db
Vishvak <vishvakr20@gmail.com>
2026-06-16 15:58:05 +0530

    Rename SaraAgent to AgentX

--- AgentX/providers/__init__.py
+++ AgentX/providers/__init__.py
@@ -1,3 +1,3 @@
-from SaraAgent.providers.base import LLMProvider, LLMResponse, ToolCallRequest
-from SaraAgent.providers.registry import ProviderSpec
-from SaraAgent.providers.openai_compat_provider import OpenaiCompactProvider
+from AgentX.providers.base import LLMProvider, LLMResponse, ToolCallRequest
+from AgentX.providers.registry import ProviderSpec
+from AgentX.providers.openai_compat_provider import OpenaiCompactProvider
